4a09ec608e6f2b0c1d3877bbb8f0a290780d4dd2
Matheus <matheuslsdn2018@gmail.com>
2024-08-16 17:21:37 -0300

    versão 1.0.3

--- 20240814/proj2/app2.py
+++ 20240814/proj2/app2.py
@@ -96,9 +96,132 @@ class proj2():
         @self.app.route('/texto_submit', methods=['POST', 'GET']) 
         def tamanho():
             texto = request.form.get('texto')
-            tamanho = texto(len)
-            return render_template('texto.html', dados=tamanho)
+            tamanho = len(texto.strip())
+            return render_template('texto.html', dados=tamanho, texto=texto)
+        
+        #atividade 12
+        @self.app.route('/calchoras')
+        def calc2():
+            return render_template('calchoras.html')
 
+        @self.app.route('/calchoras_submit', methods=['POST', 'GET'])
+        def calcsalario():
+            horas = request.form.get('horas')
+            valor = request.form.get('valor')
+            horas = horas.replace(',', '.')
+            valor = valor.replace(',', '.')
+            horas = float(horas)
+            valor = float(valor)
+            salario = horas * valor
+            salario = f'{salario:.2f}'
+            salario = salario.replace(".", ",")
+            return render_template('calchoras.html', dados={'salario':salario, 'horas':horas, 'valor':valor})
+        
+        #atividade 13
+        @self.app.route('/calcgasto')
+        def calc3():
+            return render_template('calcgasto.html')
+        
+        @self.app.route('/calcgasto_submit', methods=['POST', 'GET'])
+        def calcgasto():
+            gasolina = request.form.get('gasolina')
+            distancia = request.form.get('distancia')
+            consumo = request.form.get('consumo')
+            gasolina = gasolina.replace(',', '.')
+            distancia = distancia.replace(',', '.')
+            consumo = consumo.replace(',', '.')
+            gasolina = float(gasolina)
+            distancia = float(distancia)
+            consumo = float(consumo)
+            gasto = (distancia / consumo) * gasolina
+            gasto = f'{gasto:.2f}'
+            gasto = gasto.replace(".", ",")
+            return render_template('calcgasto.html', dados={'gasto':gasto,
+                'gasolina':gasolina,'distancia':distancia,'consumo':consumo})
+                                                            
+        #atividade 14
+        @self.app.route('/idade')
+        def idadeDias():
+            return render_template('idade.html')
+
+        @self.app.route('/idade_submit', methods=['POST', 'GET'])
+        def calc4():
+            anos = request.form.get('anos')
+            meses = request.form.get('meses')
+            dias = request.form.get('dias')
+            anos = anos.replace(',', '.')
+            meses = meses.replace(',', '.')
+            dias = dias.replace(',', '.')
+            anos = int(anos)
+            meses = int(meses)
+            dias = int(dias)
+            totalDias = anos * 365 + meses * 30 + dias
+            totalDias = f'{totalDias}'
+            return render_template('idade.html', dados={'totalDias':totalDias, 
+                                    'anos':anos, 'meses':meses, 'dias':dias})
+
+        #atividade 15
+        @self.app.route('/salario')
+        def salario():
+            return render_template('salario.html')
+        
+        @self.app.route('/salario_submit', methods=['POST', 'GET'])
+        def calc5():
+            salarioMin = request.form.get('salarioMin')
+            salarioGanho = request.form.get('salarioGanho')
+            salarioMin = salarioMin.replace(',', '.')
+            salarioGanho = salarioGanho.replace(',', '.')
+            salarioMin = float(salarioMin)
+            salarioGanho = float(salarioGanho)
+            salario = salarioGanho / salarioMin
+            salario = f'{salario:.2f}'
+            salario = salario.replace(',', '.')
+            return render_template('salario.html', dados={'salario':salario,
+                        'salarioMin':salarioMin, 'salarioGanho':salarioGanho})
+
+        #atividade 16
+        @self.app.route('/arquivo')
+        def arquivoMb():
+             return render_template('arquivo.html')
+
+        @self.app.route('/arquivo_submit', methods=['POST', 'GET'])
+        def calc6():
+            arquivo = request.form.get('arquivo')
+            velocidade = request.form.get('velocidade')
+            arquivo = arquivo.replace(',', '.')
+            velocidade = velocidade.replace(',', '.')
+            arquivo = float(arquivo)
+            velocidade = float(velocidade)
+            tempo = arquivo / velocidade
+            tempo = f'{tempo:.2f}'
+            tempo = float(tempo)
+            minutos = tempo / 60
+            minutos = f'{minutos:.2f}'
+            minutos = minutos.replace(',', '.')
+            return render_template('arquivo.html', dados={'tempo':tempo,
+            'arquivo':arquivo, 'velocidade':velocidade, 'minutos':minutos})
+        
+        #atividade 17
+        @self.app.route('/peso')
+        def pesoIdeal():
+            return render_template('peso.html')
+        
+        @self.app.route('/peso_submit', methods=['POST', 'GET'])
+        def calc7():
+            altura = request.form.get('altura')
+            peso = request.form.get('peso')
+            altura = altura.replace(',', '.')
+            peso = peso.replace(',', '.')
+            altura = float(altura)
+            peso = float(peso)
+            pesoatual = peso / (altura * 2)
+            pesoatual = f'{pesoatual:.2f}'
+            pesoatual = pesoatual.replace(',', '.')
+            pesoIdeal = (72.7 * altura) - 58
+            pesoIdeal = f'{pesoIdeal:.2f}'
+            pesoIdeal = pesoIdeal.replace(',', '.')
+            return render_template('peso.html', dados={'pesoIdeal':pesoIdeal, 
+                        'peso':peso, 'altura':altura, 'pesoatual':pesoatual})
 
 
 if __name__ == '__main__':
